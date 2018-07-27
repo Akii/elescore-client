7b473f340335ffc2217b32ff51ee1b64f5f8e2b2
Akii <zedd@akii.de>
2018-07-27 14:34:08 +0200

    Make nix file importable in overlays

--- default.nix
+++ default.nix
@@ -1,13 +1,11 @@
 with (import <nixpkgs> {});
 with (import (builtins.fetchTarball https://github.com/moretea/yarn2nix/archive/master.tar.gz) { inherit pkgs; });
-rec {
-  elescore-client = mkYarnPackage {
-    name = "elescore-client";
-    src = ./.;
-    packageJson = ./package.json;
-    yarnLock = ./yarn.lock;
-    yarnNix = ./elescore-client.nix;
-    buildPhase = "yarn build";
-    installPhase = "mv dist $out";
-  };
+mkYarnPackage {
+  name = "elescore-client";
+  src = ./.;
+  packageJson = ./package.json;
+  yarnLock = ./yarn.lock;
+  yarnNix = ./elescore-client.nix;
+  buildPhase = "yarn build";
+  installPhase = "mv dist $out";
 }
